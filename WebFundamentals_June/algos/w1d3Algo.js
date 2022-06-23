fd9c646b466f40186474ab3720b3925db843d997
TKnowland <treknowland@gmail.com>
2022-06-23 09:00:15 -0700

    For loops

---
+++ WebFundamentals_June/algos/w1d3Algo.js
@@ -0,0 +1,17 @@
+for(var j = 1; j<=5; j++){
+    if (j< 3) {
+        console.log("less than 3");
+    }
+    else if (j==3){
+        console.log("equal to 3")
+    }
+    else {
+        console.log("greater than 3");
+    }
+    
+}
+var array = ["smelly"]
+
+console.log(array[0].toUpperCase());
+
+console.log("outside of the loop. " + "After completing the loop, J is equal to " + j)
