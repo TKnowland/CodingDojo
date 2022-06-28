e025e4bbd913342280e0e53b27efd1e97fd0357a
TKnowland <treknowland@gmail.com>
2022-06-28 14:02:54 -0700

    Likes

--- WebFundamentals_June/JS/Likes/script.js
+++ WebFundamentals_June/JS/Likes/script.js
@@ -1,7 +1,21 @@
 var likeCount = 0;
-var likeElement = document.querySelector("#like_count");
+var likeCount2 = 0;
+var likeCount3 = 0;
 
-function like(element) {
+function like(element, ID) {
+    var likeElement = document.querySelector(ID);
     likeCount++;
     likeElement.innerText = likeCount + "like(s)"
+}
+
+function like2(element, ID) {
+    var likeElement = document.querySelector(ID);
+    likeCount2++;
+    likeElement.innerText = likeCount2 + "like(s)"
+}
+
+function like3(element, ID) {
+    var likeElement = document.querySelector(ID);
+    likeCount3++;
+    likeElement.innerText = likeCount3 + "like(s)"
 }
