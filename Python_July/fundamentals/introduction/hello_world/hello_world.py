4228316c7c531464e545145b62b8b0f9620037e4
TKnowland <treknowland@gmail.com>
2022-07-05 14:30:53 -0700

    Python intro

---
+++ Python_July/fundamentals/introduction/hello_world/hello_world.py
@@ -0,0 +1,14 @@
+print("Hello, world!")
+
+name = "Tre"
+fav_num = 42
+
+print ("Hello,", name)
+print ("Hello, " + name)
+print ("Hello,", fav_num)
+print ("Hello, " + str(fav_num))
+
+food_one = "fleep"
+food_two = "shneep"
+
+print(f"I love to eat {food_one} and {food_two}.")
