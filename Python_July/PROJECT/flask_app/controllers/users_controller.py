c196605f36ac88ac606124dc5f34b28db56a14a7
TKnkowland <treknowland@gmail.com>
2022-07-26 19:16:53 -0700

    Music app

---
+++ Python_July/PROJECT/flask_app/controllers/users_controller.py
@@ -0,0 +1,10 @@
+from flask_app import app
+from flask import render_template,redirect,request,session,flash
+from flask_app.models.user_model import Model
+from flask_bcrypt import Bcrypt 
+bcrypt = Bcrypt(app)
+
+
+@app.route('/')
+def display_registration():
+    return render_template('registration.html')
