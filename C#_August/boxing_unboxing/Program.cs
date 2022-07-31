63f1067979f9de59798b3bd446b954d3d52ad921
TKnowland <treknowland@gmail.com>
2022-07-30 21:24:04 -0700

    Boxing/Unboxing

---
+++ C#_August/boxing_unboxing/Program.cs
@@ -0,0 +1,19 @@
+﻿List<object> box = new List<object>();
+
+box.Add(7);
+box.Add(28);
+box.Add(-1);
+box.Add(true);
+box.Add("Chair");
+
+int sum = 0;
+
+for (int i = 0; i < box.Count; i++) {
+    Console.WriteLine(box[i]);
+    if (box[i] is int) {
+        sum += (int)box[i];
+    }
+}
+
+Console.WriteLine(sum);
+
