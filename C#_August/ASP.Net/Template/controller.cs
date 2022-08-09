056714f2144d93713e1d67b27732c9b002ecc83d
TKnkowland <treknowland@gmail.com>
2022-08-09 15:03:09 -0700

    xvbcxcvbxcvb

---
+++ C#_August/ASP.Net/Template/controller.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+namespace YourNamespace.Controllers;     //be sure to use your own project's namespace!
+    public class HelloController : Controller   //remember inheritance??
+    {
+        //for each route this controller is to handle:
+        [HttpGet]       //type of request
+        [Route("")]     //associated route string (exclude the leading /)
+        public string Index()
+        {
+            return "This is my index!";
+        }
+    }
