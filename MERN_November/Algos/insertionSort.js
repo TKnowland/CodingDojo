183bf3a0eccd60415bbfe5230bea0badc9be1b53
TKnowland <treknowland@gmail.com>
2022-10-27 08:19:04 -0700

    Insertion Sort

---
+++ MERN_November/Algos/insertionSort.js
@@ -0,0 +1,23 @@
+function insertionSort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+    for(let i = 1; i < arr.length; i++){
+        let temp = arr[i];
+        for(let j = i-1; j >= 0; j--){
+            if(temp < arr[j]){
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    console.log(arr);
+}
+
+insertionSort([3, 5, 2, 1, 0])
+insertionSort([3, 0])
+insertionSort([0])
+insertionSort([])
+insertionSort([1, 2, 3, 4, 5])
+insertionSort([5, 4, 3, 2, 1])
+insertionSort([5, 4, 0, 2, 1])
