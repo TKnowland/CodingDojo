aaaef3ca1d2bf6685f4a5d641795670b0023ca2d
TKnowland <treknowland@gmail.com>
2022-11-06 21:37:13 -0800

    mogno

---
+++ MERN_November/MongoDB/JokesAPI/server/routes/jokes.routes.js
@@ -0,0 +1,10 @@
+const JokesController = require("../controllers/jokes.controller");
+
+module.exports = app => {
+  app.get("/api/jokes", JokesController.findAllJokes);
+  app.get("/api/jokes/:_id", JokesController.findOneSingleJoke);
+  // app.get("/api/jokes/random", JokesController.)
+  app.post("/api/jokes/new", JokesController.createNewJoke);
+  app.put("/api/jokes/update/:_id", JokesController.updateExistingJoke);
+  app.delete("/api/jokes/delete/:_id", JokesController.deleteAnExistingJoke);
+};
