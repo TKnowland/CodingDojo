6ac30bbede3dbdc19e643df20e40b563dd97bb29
TKnowland <treknowland@gmail.com>
2022-11-09 12:59:24 -0800

    product manager

--- MERN_November/fullstackMERN/ProductManager/server/controllers/products.controller.js
+++ MERN_November/fullstackMERN/ProductManager/server/controllers/products.controller.js
@@ -7,7 +7,7 @@ module.exports.getAll = (req, res) => {
         .catch(err => res.json({ message: 'Something went wrong', error: err }));
 }
  
-module.exports.findOneSingleProduct = (req, res) => {
+module.exports.getOne = (req, res) => {
     Product.findOne({ _id: req.params.id })
         .then(oneSingleProduct => res.json({ product: oneSingleProduct }))
         .catch(err => res.json({ message: 'Something went wrong', error: err }));
@@ -20,7 +20,7 @@ module.exports.create = (req, res) => {
         .catch(err => res.json({ message: 'Something went wrong', error: err }));
 }
  
-module.exports.updateExistingProduct = (req, res) => {
+module.exports.update = (req, res) => {
     Product.findOneAndUpdate(
         { _id: req.params.id },
         req.body,
@@ -30,7 +30,7 @@ module.exports.updateExistingProduct = (req, res) => {
         .catch(err => res.json({ message: 'Something went wrong', error: err }));
 }
  
-module.exports.deleteAnExistingProduct = (req, res) => {
+module.exports.delete = (req, res) => {
     Product.deleteOne({ _id: req.params.id })
         .then(result => res.json({ result: result }))
         .catch(err => res.json({ message: 'Something went wrong', error: err }));
--- MERN_November/fullstackMERN/ProductManager/server/routes/product.routes.js
+++ MERN_November/fullstackMERN/ProductManager/server/routes/product.routes.js
@@ -1,9 +1,9 @@
 const productsController = require('../controllers/products.controller');
 module.exports = function(app){
     app.get('/api/products', productsController.getAll);
-    // app.get('/api/products/:id', productsController.findOneSingleProduct);
+    app.get('/api/products/:id', productsController.findOneSingleProduct);
     app.post('/api/products', productsController.create);
-    // app.put('/api/products/update/:id', productsController.updateExistingProduct);
-    // app.delete('/api/products/delete/:id', productsController.deleteAnExistingProduct);
+    app.put('/api/products/update/:id', productsController.update);
+    app.delete('/api/products/delete/:id', productsController.delete);
 }
 
