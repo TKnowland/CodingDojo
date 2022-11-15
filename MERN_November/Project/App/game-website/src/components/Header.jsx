81691377a1ad1c4f9ffb61e1e91f1e2004caee19
TKnowland <treknowland@gmail.com>
2022-11-15 11:37:27 -0800

    home page

--- MERN_November/Project/App/game-website/src/components/Header.jsx
+++ MERN_November/Project/App/game-website/src/components/Header.jsx
@@ -5,13 +5,13 @@ import styles from './Header.module.css'
 export default () => {
   return (
     <div className={styles.header}>
-      <h1>Metroidvania Project</h1>
+      <h1><Link to={'/'}>Metroidvania Project</Link></h1>
       <div>
         <ul className={styles.nav}>
           <li><a href="https://github.com/TKnowland/MetroidvaniaProject">GitHub</a></li>
           <li>ChangeLog</li>
           <li><a href="https://tknowland.itch.io/metroidvania">Demo</a></li>
-          <li><Link>About</Link></li>
+          <li><Link to={'/about'}>About</Link></li>
         </ul>
       </div>
     </div>
--- MERN_November/Project/App/game-website/src/views/About.jsx
+++ MERN_November/Project/App/game-website/src/views/About.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => {
   return (
-    <div>Slideshow</div>
+    <div>About</div>
   )
 }
