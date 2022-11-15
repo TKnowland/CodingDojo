a2c237f8f525c4fca3c607a1c4d6dc33c0c007d7
TKnowland <treknowland@gmail.com>
2022-11-15 10:25:53 -0800

    blah

--- MERN_November/Project/App/game-website/src/components/Header.jsx
+++ MERN_November/Project/App/game-website/src/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from './Header.module.css'
 
 export default () => {
   return (
-    <div>Header</div>
+    <div className={styles.header}>
+      <h1>Metroidvania Project</h1>
+      <div>
+        <ul className={styles.nav}>
+          <li><a href="https://github.com/TKnowland/MetroidvaniaProject">GitHub</a></li>
+          <li>ChangeLog</li>
+          <li><a href="https://tknowland.itch.io/metroidvania">Demo</a></li>
+          <li><Link>About</Link></li>
+        </ul>
+      </div>
+    </div>
   )
 }
